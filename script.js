ffa5c3a828ba1d4d929252889d34a6246709c09e
vladimir-tomic <vladimir.tomic0209@gmail.com>
2020-10-11 18:31:19 +0200

    copy/paste

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.querySelector(".hamburger-menu").addEventListener("click", () => {
-    document.querySelector(".container").classList.toggle("change");
-  });
-
   
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
 document.querySelector(".scroll-btn").addEventListener("click", () => {
   document.querySelector("html").style.scrollBehavior = "smooth";
   setTimeout(() => {
